58919fb6f5e45a0d09da5bab4dfe5b3d856bd2a2
Gildrom <forrai.zoltan66@gmail.com>
2026-07-09 16:46:08 +0200

    fix warble icon colors and links

--- SRC/js/warbler.js
+++ SRC/js/warbler.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
           </svg>
         </label>
-        <a href="/tavern/contact">
+        <a href="/beyond/contact">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719" />
           </svg>
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     checkbox.addEventListener("change", () => {
       const heartStates = JSON.parse(
-        localStorage.getItem("heartStates") || "{}"
+        localStorage.getItem("heartStates") || "{}",
       );
       if (checkbox.checked) {
         heartStates[uniqueId] = true;
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.addEventListener("load", () => {
   const firstPost = document.querySelector(
-    "tweet-timeline status-update:first-of-type"
+    "tweet-timeline status-update:first-of-type",
   );
   if (!firstPost) return;
 
@@ -95,7 +95,7 @@ window.addEventListener("load", () => {
     const fullHeight = firstPost.offsetHeight + marginTop + marginBottom;
     document.documentElement.style.setProperty(
       "--first-post-height",
-      `-${fullHeight}px`
+      `-${fullHeight}px`,
     );
 
     const muted = localStorage.getItem("audioMuted") === "true";
